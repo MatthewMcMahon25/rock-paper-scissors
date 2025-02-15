14a92e6acc06401828824f506bbd0436871409e2
MatthewMcMahon25 <macmatt5@gmail.com>
2025-02-15 15:31:24 -0500

    Create getComputerChoice
    Built a function that generates a random number,
    then converts that random number into either
    rock, paper or scissors. Then returns that choice.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,35 @@
 console.log("Hello World");
+
+// Gets a random number 0, 1 or 2
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3);
+}
+
+// Gets the computers choice and returns either Rock, Paper or Scissors
+function getComputerChoice() {
+    let choice;
+
+    // Creates a random number: 0 (Rock), 1 (Paper) or 2 (Scissors)
+    randomNum = getRandomNumber();
+    console.log(randomNum);
+
+    // If the number equals 0, the computer's choice is Rock
+    if (randomNum === 0) {
+        choice = "Rock";
+    }
+    // If the number equals 1, the computer's choice is Paper
+      else if (randomNum === 1) {
+        choice = "Paper";
+    }
+
+    // Else the number equals 2, the computer's choice is Scissors
+      else {
+        choice = "Scissors";
+      }
+
+    // Returns the computer's choice
+    console.log(choice);
+    return choice;
+}
+
+getComputerChoice();
